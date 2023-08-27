4ec272d199c4c1c3a7dbdf6babc241fa960b2d0a
MaciejKamianowski <maciek-kamianowski@o2.pl>
2023-08-27 14:43:28 +0200

    Add simple c++20 example

--- Playground_CMake/Application.cpp
+++ Playground_CMake/Application.cpp
@@ -3,4 +3,10 @@
 int main(int argc, char** argv) 
 {
 	std::cout << "Hello Cmake world!" << std::endl;
+    if constexpr (std::endian::native == std::endian::big)
+        std::cout << "big-endian\n";
+    else if constexpr (std::endian::native == std::endian::little)
+        std::cout << "little-endian\n";
+    else
+        std::cout << "mixed-endian\n";
 }
